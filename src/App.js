1c6897d5d7f97bdc8969de5a134205e1253d8710
sharonc05 <sharonxc@g.ucla.edu>
2024-02-14 19:26:12 -0800

    first commit: added a random number generator button. Should be replaced with random DJ names later

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
 // Make sure the import path matches the location of radioblack.png
 import radioBlack from './images/radioblack.png';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("Click on the button to generate your DJ Name!");
+  const user = "DJ ";
+
+  const generateRandomName = () => {
+    // Generate a random number and use it as part of the name
+    const randomNumber = Math.floor(Math.random() * 100); // Example: Generate a number between 0 and 99
+    const randomName = `${user}${randomNumber}`; // Concatenate 'user_' with the random number
+    setName(randomName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={radioBlack} className="App-logo" alt="Radio Black" />
-        Hi - Scott
-        
-      </header>
-    </div>
+    <><div className="App">
+      <div>
+        <button onClick={generateRandomName}>Generate DJ Name</button>
+        <p>{name}</p> {/* Display the generated name */}
+      </div>
+    </div><div className="App">
+        <header className="App-header">
+          <img src={radioBlack} className="App-logo" alt="Radio Black" />
+
+
+        </header>
+      </div></>
   );
-}
+
+};
+
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={radioBlack} className="App-logo" alt="Radio Black" />
+        
+        
+    //   </header>
+    // </div>
+//   );
+// }
 
 export default App;

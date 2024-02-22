05e7d8abe1cbaaf9c6a37fa656f05b14f0b8ee61
charlesleelong <charles.lee.long@gmail.com>
2024-02-22 15:52:01 -0800

    change
    added chance to use text file name

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
 import './App.css';
 import Typewriter from "./Typewriter";
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   // VARIABLES/SETTER FUNCTIONS
-  const [adj, setAdj] = useState("");
-  const [noun, setNoun] = useState("");
+  const [Name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [keyProp, setTypewriterKey] = useState(0);
 
-  //RESETS THE TYPEWRITER 
+  // RESETS THE TYPEWRITER 
   const resetTypewriter = () => {
     setTypewriterKey(keyProp => keyProp + 1);
   };
 
-  function fetchName() {
-    //HOSTING THIS ON AN AWS LIGHSTAIL INSTANCE: https://github.com/mcnaveen/Random-Words-API
-    fetch('http://35.89.90.194:3000/word/adjective/')
-      .then(response => response.json())
-      .then(data1 => {
-        setAdj(data1[0].word);
-        return fetch('http://35.89.90.194:3000/word/noun/');
-      })
-      .then(response => response.json())
-      .then(data2 => {
-        setNoun(data2[0].word);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
+  async function fetchName() {
+    setLoading(true);
+    try {
+      let response = await fetch('http://cat.epicgamer.org:3000/word/adjective/');
+      let data1 = await response.json();
+
+      response = await fetch('http://cat.epicgamer.org:3000/word/noun/');
+      let data2 = await response.json();
+
+      setName(`DJ ${data1[0].word} ${data2[0].word}`);
+      resetTypewriter();
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }
+  
+  async function getRandomLine() {
+    try {
+      const response = await fetch('/djnames.txt');
+      const text = await response.text();
+      const lines = text.split(' ');
+      const randomLine = lines[Math.floor(Math.random() * lines.length)];
+      setName(`DJ ${randomLine}`);
+      resetTypewriter();
+    } catch (error) {
+      console.error('Error fetching the names file:', error);
+      setError(error);
+    }
   }
 
-  //API CALL AND TYPEWRITER REMOUNT
+  // API CALL
   const buttonClick = () => {
-    fetchName();
-    resetTypewriter();
+    if (Math.random() < 0.5) {
+      getRandomLine();
+    }
+    else {
+      fetchName();
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>YOUR DJ NAME IS: </p>
-        <button onClick={buttonClick}>GET ME A NAME</button>
+        <button onClick={buttonClick} disabled={loading}>GET ME A NAME</button>
         {error && <p>Error: {error.message}</p>}
-        {(
-          <h1>
-            {`DJ ${adj} ${noun}`}
-            {/*<Typewriter key={keyProp} text={`${adj} ${noun}`} delay={100} />*/}
-          </h1>
-        )}
+        <h1>
+          {Name}
+          {/* Uncomment the Typewriter component when it's ready to be used */}
+          {/* <Typewriter key={keyProp} text={Name} delay={100} /> */}
+        </h1>
       </header>
     </div>
   );
